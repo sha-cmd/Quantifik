c587f5d38813d8a2c4bbbb6180034da2df679750
romain Boyrie <romain@boyrie.email>
2021-09-09 11:36:49 +0200

    Partie 2 Machine learning.

---
+++ src/model_selection/Select_KBest.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest, chi2
+
+
+def get_default_params():
+    return pd.DataFrame.from_dict({'k': 5})
+
+
+class Select_KBest:
+    
+    def __init__(self, X, y, params: pd.DataFrame = get_default_params()):
+        scaler = MinMaxScaler()
+        scaler.fit(X)
+        X_plus = scaler.transform(X)
+        selector = SelectKBest(chi2, k=params['k'])
+        selector.fit(X_plus, y)
+        X_new = selector.transform(X)
+        X_new.shape
+        return [list(X.columns)[it] for it, x in enumerate(selector.get_support().tolist()) if x]
+        
