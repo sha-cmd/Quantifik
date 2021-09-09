35a0a70e106f60a970ac7f2b1e11fade04050c00
romain Boyrie <romain@boyrie.email>
2021-09-09 12:58:37 +0200

    Partie 2 Machine learning.

--- src/algorithms/RandomForestClassifier.py
+++ src/algorithms/RandomForestClassifier.py
@@ -27,8 +27,10 @@ def default_params():
 
 class RandomForestClassifier(Algorithm):
 
-    def __init__(self, name, params: pd.DataFrame = default_params()):
+    def __init__(self, X, y, params: pd.DataFrame = default_params()):
         super(RandomForestClassifier, self).__init__(name)
+
+        self.clf: RandomForestClassifier = RandomForestClassifier
         for it, row in params.iterrows():
             #self.type = row['Type']
             self.n_estimators = row['n_estimators']
@@ -50,9 +52,36 @@ class RandomForestClassifier(Algorithm):
             self.class_weight = row['class_weight']
             self.ccp_alpha = row['ccp_alpha']
             self.max_samples = row['max_samples']
+        self.make_classifier()
 
     def gen_df_params(self):
         default_params().to_csv('RandomForestClassifier_params.csv')
+
+    def make_classifier(self):
+        self.clf = RandomForestClassifier(n_estimators=self.n_estimators,
+                                         criterion=self.criterion,
+                                         max_depth=self.max_depth,
+                                         min_samples_split=self.min_samples_split,
+                                         min_samples_leaf=self.min_samples_leaf,
+                                         min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+                                         max_features=self.max_features,
+                                         max_leaf_nodes=self.max_leaf_nodes,
+                                         min_impurity_decrease=self.min_impurity_decrease,
+                                         min_impurity_split=self.min_impurity_split,
+                                         bootstrap=self.bootstrap,
+                                         oob_score=self.oob_score,
+                                         n_jobs=self.n_jobs,
+                                         random_state=self.random_state,
+                                         verbose=self.verbose,
+                                         warm_start=self.warm_start,
+                                         class_weight=self.class_weight,
+                                         ccp_alpha=self.ccp_alpha,
+                                         max_samples=self.max_samples
+                                         )
+
+    def transform(self, X, y):
+        self.clf.fit(X,y)
+        return self.clf.transform(X, y)
 
     def __str__(self):
         return self.name
