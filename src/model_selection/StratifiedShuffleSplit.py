6e5b93ecfb056698e2e0c0dcd1790a843a086d51
romain Boyrie <romain@boyrie.email>
2021-09-09 19:01:20 +0200

    Partie 2 Machine learning.

--- src/model_selection/StratifiedShuffleSplit.py
+++ src/model_selection/StratifiedShuffleSplit.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit as SSS
 
@@ -16,7 +15,6 @@ class StratifiedShuffleSplit:
                   test_size=params['test_size'][0],
                   random_state=params['random_state'][0].astype(int))
         sss.get_n_splits(X, y)
-        print(params.dtypes)
         for train_index, test_index in sss.split(X, y):
             self.X_train, self.X_test = X.iloc[train_index], X.iloc[test_index]
             self.y_train, self.y_test = y.iloc[train_index], y.iloc[test_index]
