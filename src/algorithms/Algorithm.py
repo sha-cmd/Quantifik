cd6c69e590df73feac0e023e42e246590567e864
romain Boyrie <romain@boyrie.email>
2021-09-10 19:16:44 +0200

    Cleaning code

--- src/algorithms/Algorithm.py
+++ src/algorithms/Algorithm.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import shap
 
 from src.utils.logger import log_init as log
 from src.algorithms.RandomForestClassifier import RandomForestClassifierAlgorithm
@@ -15,6 +17,7 @@ class Algorithm:
         self.name = name
 
     def ml_algo_RandomForest(self, X, y, nom):
+        print(nom)
         params_csv = default_df()
 
         os.chdir('..')
@@ -32,18 +35,28 @@ class Algorithm:
                 for it, col in row.iteritems():
                     col = None if col == 'nan' else col
                     df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
-
-                c: RandomForestClassifierAlgorithm = RandomForestClassifierAlgorithm(b.X_train.to_numpy(),
-                                                                                     b.y_train.iloc[:, 0].ravel(), row)
-                c.fit_clf(b.X_train, b.y_train)
                 nom = 'RandomForest'
                 num = str(int(it_i))
 
                 dossier = nom + '_' + num + '_' + 'KBest_' + str(row_kbest['k'])
+                print('\n***************\n' + dossier)
+                c: RandomForestClassifierAlgorithm = RandomForestClassifierAlgorithm(b.X_train.to_numpy(),
+                                                                                     b.y_train.iloc[:, 0].ravel(), row)
+                c.fit_clf(b.X_train, b.y_train)
+
                 if dossier not in os.listdir():
                     os.mkdir(dossier)
                 c.metrics(b.X_test, b.y_test, dossier, nom, 'jpg')
                 c.permutation_importances(b.X_train, b.y_train, 'Train set', dossier, nom, 'jpg')
+                # print('shap')
+                # explainer = shap.TreeExplainer(c.clf)
+                #
+                # shap_values = explainer(X)
+                # shap.plots.beeswarm(shap_values)
+                # plt.savefig('beeswarm.jpg')
+                # shap.plots.bar(shap_values)
+                # plt.savefig('bar.jpg')
+                # plt.close()
 
     def ml_algo_LogisticRegression(self, X, y, nom):
         params_csv = default_df()
@@ -63,15 +76,19 @@ class Algorithm:
                 for it, col in row.iteritems():
                     col = None if col == 'nan' else col
                     df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
-
-                c: LogisticRegressionAlgorithm = LogisticRegressionAlgorithm(b.X_train.to_numpy(),
-                                                                             b.y_train.iloc[:, 0].ravel(), row)
-                c.fit_clf(b.X_train, b.y_train)
                 nom = 'LogisticRegression'
                 num = str(int(it_i))
 
                 dossier = nom + '_' + num + '_' + 'KBest_' + str(row_kbest['k'])
+                print('\n***************\n' + dossier)
+                c: LogisticRegressionAlgorithm = LogisticRegressionAlgorithm(b.X_train.to_numpy(),
+                                                                             b.y_train.iloc[:, 0].ravel(), row)
+                c.fit_clf(b.X_train, b.y_train)
+
                 if dossier not in os.listdir():
                     os.mkdir(dossier)
                 c.metrics(b.X_test, b.y_test, dossier, nom, 'jpg')
-                c.permutation_importances(b.X_train, b.y_train, 'Train set', dossier, nom, 'jpg')
+                #print('permutation')
+                #c.permutation_importances(b.X_train, b.y_train, 'Train set', dossier, nom, 'jpg')
+
+
