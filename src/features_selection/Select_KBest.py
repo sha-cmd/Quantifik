443fe16af852b60fa418286ef51c22a42b7a5838
romain Boyrie <romain@boyrie.email>
2021-09-10 15:00:01 +0200

    Renaming

--- src/features_selection/Select_KBest.py
+++ src/features_selection/Select_KBest.py
@@ -7,6 +7,16 @@ from sklearn.feature_selection import SelectKBest, chi2
 def get_default_params():
     return pd.DataFrame.from_dict({'k': 5}, orient='index').T
 
+def get_params_from_file(nom_fichier):
+    params_csv = pd.DataFrame()
+    df = pd.DataFrame({'k': pd.Series([], dtype=int)})
+    params_csv = params_csv.append(pd.read_csv(nom_fichier, index_col='index'))
+    for it_i, row in params_csv.iterrows():
+        for it, col in row.iteritems():
+            col = None if col == 'nan' else col
+            df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
+    df['k'] = df['k'].astype(int)
+    return df
 
 class Select_KBest:
     
@@ -14,9 +24,9 @@ class Select_KBest:
         scaler = MinMaxScaler()
         scaler.fit(X)
         X_plus = scaler.transform(X)
-        selector = SelectKBest(chi2, k=params['k'][0])
+        selector = SelectKBest(chi2, k=params['k'])
         selector.fit(X_plus, y)
         X_new = selector.transform(X)
         X_new.shape
-        self.columns =  [list(X.columns)[it] for it, x in enumerate(selector.get_support().tolist()) if x]
+        self.columns = [list(X.columns)[it] for it, x in enumerate(selector.get_support().tolist()) if x]
         
--- src/features_selection/__init__.py
+++ src/features_selection/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ['Select_KBest']
+__all__ = ['Select_KBest', 'get_params_from_file']
 
 from .Select_KBest import Select_KBest
+from .Select_KBest import get_params_from_file
