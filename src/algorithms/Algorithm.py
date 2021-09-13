384acdb760ee56da7a85d2701ce57c8607815dfe
romain Boyrie <romain@boyrie.email>
2021-09-13 15:07:15 +0200

    Transformation de copie

--- src/algorithms/Algorithm.py
+++ src/algorithms/Algorithm.py
@@ -7,6 +7,7 @@ import shap
 from src.utils.logger import log_init as log
 from src.algorithms.RandomForestClassifier import RandomForestClassifierAlgorithm
 from src.algorithms.LogisticRegressionAlgorithm import LogisticRegressionAlgorithm
+from src.algorithms.HistGradientBoostingAlgorithm import HistGradientBoostingAlgorithm
 from src.features_selection import Select_KBest, get_params_from_file
 from src.model_selection import StratifiedShuffleSplit
 from src.algorithms.RandomForestClassifier import default_df
@@ -35,7 +36,7 @@ class Algorithm:
                 for it, col in row.iteritems():
                     col = None if col == 'nan' else col
                     df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
-                nom = 'RandomForest'
+                #nom = 'RandomForest'
                 num = str(int(it_i))
 
                 dossier = nom + '_' + num + '_' + 'KBest_' + str(row_kbest['k'])
@@ -58,6 +59,7 @@ class Algorithm:
                 # plt.savefig('bar.jpg')
                 # plt.close()
 
+
     def ml_algo_LogisticRegression(self, X, y, nom):
         params_csv = default_df()
 
@@ -76,7 +78,7 @@ class Algorithm:
                 for it, col in row.iteritems():
                     col = None if col == 'nan' else col
                     df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
-                nom = 'LogisticRegression'
+                #nom = 'LogisticRegression'
                 num = str(int(it_i))
 
                 dossier = nom + '_' + num + '_' + 'KBest_' + str(row_kbest['k'])
@@ -90,5 +92,39 @@ class Algorithm:
                 c.metrics(b.X_test, b.y_test, dossier, nom, 'jpg')
                 #print('permutation')
                 #c.permutation_importances(b.X_train, b.y_train, 'Train set', dossier, nom, 'jpg')
+
+
+    def ml_algo_HistGradientBoosting(self, X, y, nom):
+        params_csv = default_df()
+
+        os.chdir('..')
+        nom_fichier = 'HistGradientBoostingClassifier_params.csv'
+        params_csv = params_csv.append(pd.read_csv(nom_fichier, index_col='index'))
+        fichier_sel_k_best = 'Select_KBest_params.csv'
+        select_k_best_csv = get_params_from_file(fichier_sel_k_best)
+        os.chdir('AutoML')
+        df = default_df()
+
+        for it_kbest, row_kbest in select_k_best_csv.iterrows():
+            a = Select_KBest(X, y, row_kbest)
+            b = StratifiedShuffleSplit(X[a.columns], y)
+            for it_i, row in params_csv.iterrows():
+                for it, col in row.iteritems():
+                    col = None if col == 'nan' else col
+                    df.at[it_i, it] = pd.to_numeric(col, errors='ignore')
+                #nom = 'HistGradientBoosting'
+                num = str(int(it_i))
+
+                dossier = nom + '_' + num + '_' + 'KBest_' + str(row_kbest['k'])
+                print('\n***************\n' + dossier)
+                c: HistGradientBoostingAlgorithm = HistGradientBoostingAlgorithm(b.X_train.to_numpy(),
+                                                                             b.y_train.iloc[:, 0].ravel(), row)
+                c.fit_clf(b.X_train, b.y_train)
+
+                if dossier not in os.listdir():
+                    os.mkdir(dossier)
+                c.metrics(b.X_test, b.y_test, dossier, nom, 'jpg')
+                # print('permutation')
+                # c.permutation_importances(b.X_train, b.y_train, 'Train set', dossier, nom, 'jpg')
 
 
